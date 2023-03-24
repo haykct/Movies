546e83eaaaf67c9d1e05fa94ba011a59b1475a41
haykct <https://github.com/HaykoHayrapetyan>
2023-03-24 18:53:03 +0100

    Code clean up.

--- Movies/Coordinators/ShowsCoordinator.swift
+++ Movies/Coordinators/ShowsCoordinator.swift
@@ -23,10 +23,10 @@ final class ShowsCoordinator: Coordinator {
     //MARK: methods
     
     func start() {
-        let swiftUIViewController = ShowsHostingViewController(rootView: ShowsView(viewModel: ShowsViewModel(networkService: DefaultNetworkService())))
+        let hostingViewController = ShowsHostingViewController(rootView: ShowsView(viewModel: ShowsViewModel(networkService: DefaultNetworkService())))
         
-        swiftUIViewController.tabBarItem = UITabBarItem(title: "Shows", image: UIImage(systemName: "tv.inset.filled"), tag: 1)
-        navigationController.setViewControllers([swiftUIViewController], animated: false)
+        hostingViewController.tabBarItem = UITabBarItem(title: "Shows", image: UIImage(systemName: "tv.inset.filled"), tag: 1)
+        navigationController.setViewControllers([hostingViewController], animated: false)
     }
     
 }
