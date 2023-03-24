03b416e5a8b0bd6bd805380b7a34286fa9cccb0d
haykct <https://github.com/HaykoHayrapetyan>
2023-03-24 19:25:31 +0100

    added BaseConfiguration.swift

---
+++ Movies/Movies/ViewControllers/Configuration/BaseConfiguration.swift
@@ -0,0 +1,56 @@
+//
+//  BaseViewController.swift
+//  Movies
+//
+//  Created by Hayk Hayrapetyan on 24.03.23.
+//
+
+import UIKit
+
+protocol BaseConfiguration {
+    var navigationTitle: String? { get }
+    
+    func setupTabBarItem()
+    func setupNavigationBar()
+}
+
+extension BaseConfiguration where Self: UIViewController {
+    
+    private typealias NunitoSans = Constants.Fonts.NunitoSans
+    
+    var navigationTitle: String? { nil }
+    
+    func setupTabBarItem() {
+        let tabBarAppearance = UITabBarAppearance()
+        let titleFontSize: CGFloat = 14
+        let font = UIFont(name: NunitoSans.semiBold, size: titleFontSize) as Any
+
+        tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.font: font]
+        tabBarItem.standardAppearance = tabBarAppearance
+        tabBarItem.scrollEdgeAppearance = tabBarAppearance
+    }
+    
+    
+    func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        let titleFontSize: CGFloat = 20
+        let largeTitleFontSize: CGFloat = 32
+        let buttonTitleFontSize: CGFloat = 18
+        let image = Constants.Images.System.filledCircle
+        let color = Constants.Colors.grey as Any
+        let titleFont = UIFont(name: NunitoSans.black, size: titleFontSize) as Any
+        let largeTitleFont = UIFont(name: NunitoSans.black, size: largeTitleFontSize) as Any
+        let buttonTitleFont = UIFont(name: NunitoSans.bold, size: buttonTitleFontSize) as Any
+        
+        navigationItem.title = navigationTitle
+        navigationController?.navigationBar.prefersLargeTitles = true
+        appearance.titleTextAttributes = [.foregroundColor: color, .font: titleFont]
+        appearance.largeTitleTextAttributes = [.foregroundColor: color, .font: largeTitleFont]
+        appearance.buttonAppearance.normal.titleTextAttributes = [.foregroundColor: color,
+                                                                  .font: buttonTitleFont]
+        appearance.setBackIndicatorImage(image, transitionMaskImage: image)
+        navigationController?.navigationBar.tintColor = Constants.Colors.grey
+        navigationController?.navigationBar.standardAppearance = appearance
+    }
+
+}
